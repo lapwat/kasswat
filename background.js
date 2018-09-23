888652ad456715812eb386c961a11b159b7a0c4d
Quentin <q.lapointe@gmail.com>
2018-09-23 23:49:07 +0200

    updated remove rules, add Amazon website

--- background.js
+++ background.js
@@ -5,12 +5,29 @@ function formatDate(date) {
   return hour + ':' + min + ':' + sec
 }
 
+// facebook
 $("#pagelet_ego_pane").remove()
 
+// amazon
+$("#sx-top-slot").remove()
+$(".s-result-item:contains('Sponsorisé')").remove()
+$(".s-result-item:contains('Sponsored')").remove()
+$(".s-result-item:contains('Best Sellers from popular brands')").remove()
+$(".s-result-item:contains('Top Rated from Our Brands')").remove()
+$(".s-result-item:contains('Expert Recommendations')").remove()
+$(".s-result-item:contains('Video buying guides')").remove()
+$(".s-result-item:contains('Amazon\\'s')").remove()
+
+// google
+$("#tads").remove()
+$("#tvcaps").remove()
+$("#rhs").remove()
+
 (function(){
-    var facebook = $("div[data-fte='1']:not(:has(.timestampContent))")
-    var google = $("#tvcap")
-    facebook.remove()
-    google.remove()
+    var facebook_fr = $("div[data-fte='1']:contains('SpSonsSoriSséS')")
+    var facebook_en = $("div[data-fte='1']:contains('SpSonSsoSredS')")
+
+    facebook_fr.remove()
+    facebook_en.remove()
     setTimeout(arguments.callee, 5000)
 })()
